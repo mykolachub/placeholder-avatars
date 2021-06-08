e5762dd65518656f2171e9b48e9db92115abd60d
Nikolai Chub <nikolaichub.me@gmail.com>
2021-06-08 20:10:51 +0300

    CommonJS replaced with ES Modules, /assets added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-'use strict';
+import Placeholder from './lib/placeholder.js';
+
+const placeholder = new Placeholder({
+  type: 'square',
+  size: 512,
+  variant: 'grid',
+  elements: 'square',
+  amount: 9,
+  density: 8,
+});
+
+const plchlrd = placeholder.generate();
+
+const svg = document.getElementById('svg');
+svg.innerHTML = plchlrd;
